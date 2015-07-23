4385a05bf75b0d910e30e04ce443aa1a1afb37fc
Petar Ivancevic <petar.ivancevic171@gmail.com>
2015-07-23 21:41:07 +0200

    Završni rad
    Cijeli kod igre.
    Skripte se nalaze pod Assets, kao i ostale važne datoteke.
    
    Dodat ću jednog dana bolji opis...

---
+++ Assets/Scripts/CarMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class CarMovement : MonoBehaviour {
+
+	//podatkovni clanovi klase za kretanje autica
+	//kola i pivoti, pivoti se koriste za okretanje kola
+	private GameObject LeftBackTire;
+	private GameObject LeftBackTirePivot;
+	private GameObject RightBackTire;
+	private GameObject RightBackTirePivot;
+	private GameObject LeftFrontTire;
+	private GameObject LeftFrontTirePivot;
+	private GameObject RightFrontTire;
+	private GameObject RightFrontTirePivot;
+
+	//ova se metoda pokrece na pocetku 
+	//NIJE KONSTRUKTOR i ne smiju se koristiti
+	public void Start() {
+		this.LeftBackTire = GameObject.Find("simpleCar/LeftBackTirePivot/LeftBackTire");
+		this.LeftBackTirePivot = GameObject.Find ("simpleCar/LeftBackTirePivot");
+		this.RightBackTire = GameObject.Find("simpleCar/RightBackTirePivot/RightBackTire");
+		this.RightBackTirePivot = GameObject.Find("simpleCar/RightBackTirePivot");
+		this.LeftFrontTire = GameObject.Find("simpleCar/LeftFrontTirePivot/LeftFrontTire");
+		this.LeftFrontTirePivot = GameObject.Find("simpleCar/LeftFrontTirePivot");
+		this.RightFrontTire = GameObject.Find("simpleCar/RightFrontTirePivot/RightFrontTire");
+		this.RightFrontTirePivot = GameObject.Find("simpleCar/RightFrontTirePivot");
+	}
+
+	//ova se metoda poziva svaki frame
+	public void FixedUpdate()
+	{
+		float move = Input.GetAxis ("Horizontal");
+
+		this.LeftBackTire.transform.RotateAround (this.LeftBackTirePivot.transform.position, Vector3.right, 50*Time.deltaTime); 
+	}
+}
+
