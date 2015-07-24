c351a333abd3d074ea5390b36e523c76709d04ab
Petar Ivancevic <petar.ivancevic171@gmail.com>
2015-07-24 19:13:49 +0200

    Prepravljeno kretanje automobila i pravilno okretanje kola

--- Assets/Scripts/CarMovement.cs
+++ Assets/Scripts/CarMovement.cs
@@ -5,36 +5,65 @@ using System.Collections.Generic;
 
 public class CarMovement : MonoBehaviour {
 
+
 	//podatkovni clanovi klase za kretanje autica
-	//kola i pivoti, pivoti se koriste za okretanje kola
-	private GameObject LeftBackTire;
-	private GameObject LeftBackTirePivot;
-	private GameObject RightBackTire;
-	private GameObject RightBackTirePivot;
-	private GameObject LeftFrontTire;
-	private GameObject LeftFrontTirePivot;
-	private GameObject RightFrontTire;
-	private GameObject RightFrontTirePivot;
+	public float speed = 15.0f;
+	/*	Kola i tijelo auta moraju biti sinkronizirana
+	 *  inace će se kola vrtiti krivo
+	 * */
+	private Quaternion wheelRotationToBody;
+
+	//svi kotaci u string zapisu
+	private string[] tireNames = new string[4] {
+		"simpleCar/LeftFrontTirePivot/LeftFrontTire",
+		"simpleCar/RightFrontTirePivot/RightFrontTire",
+		"simpleCar/LeftBackTirePivot/LeftBackTire",
+		"simpleCar/RightBackTirePivot/RightBackTire",
+	};
+
+	//svi pivoti u string zapisu
+	private string[] tirePivotNames = new string[4]{
+		"simpleCar/LeftFrontTirePivot",
+		"simpleCar/RightFrontTirePivot",
+		"simpleCar/LeftBackTirePivot",
+		"simpleCar/RightBackTirePivot"
+	};
+	//niz kotaca i pivota
+	private GameObject[] tires;
+	private GameObject[] tirePivots;
+
 
 	//ova se metoda pokrece na pocetku 
 	//NIJE KONSTRUKTOR i ne smiju se koristiti
 	public void Start() {
-		this.LeftBackTire = GameObject.Find("simpleCar/LeftBackTirePivot/LeftBackTire");
-		this.LeftBackTirePivot = GameObject.Find ("simpleCar/LeftBackTirePivot");
-		this.RightBackTire = GameObject.Find("simpleCar/RightBackTirePivot/RightBackTire");
-		this.RightBackTirePivot = GameObject.Find("simpleCar/RightBackTirePivot");
-		this.LeftFrontTire = GameObject.Find("simpleCar/LeftFrontTirePivot/LeftFrontTire");
-		this.LeftFrontTirePivot = GameObject.Find("simpleCar/LeftFrontTirePivot");
-		this.RightFrontTire = GameObject.Find("simpleCar/RightFrontTirePivot/RightFrontTire");
-		this.RightFrontTirePivot = GameObject.Find("simpleCar/RightFrontTirePivot");
+		this.tires = new GameObject[4];
+		this.tirePivots = new GameObject[4];
+
+		for (int i = 0; i < 4; i++) {
+			this.tires[i] = GameObject.Find(this.tireNames[i]);
+			this.tirePivots[i] = GameObject.Find(this.tirePivotNames[i]);
+		}
+	}
+
+
+	private void RotateTheTires(float move) {
+		move = move * 25f;
+		for(int i = 0; i < 4; i++)
+			this.tirePivots[i].transform.Rotate (0f, 0f, this.speed * move * Time.deltaTime);
 	}
 
 	//ova se metoda poziva svaki frame
 	public void FixedUpdate()
 	{
-		float move = Input.GetAxis ("Horizontal");
+		float move = Input.GetAxis ("Vertical");
+		//float steer = Input.GetAxis ("Horizontal");
 
-		this.LeftBackTire.transform.RotateAround (this.LeftBackTirePivot.transform.position, Vector3.right, 50*Time.deltaTime); 
+		Vector3 movement = Vector3.forward * move * this.speed * Time.deltaTime;
+
+		this.transform.Translate (movement);
+		this.RotateTheTires (-move);
 	}
+
+
 }
 
