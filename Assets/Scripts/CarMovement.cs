5c989041dac21fee58d522c00cb0e003aacad6fe
Petar Ivancevic <petar.ivancevic171@gmail.com>
2015-07-28 15:50:44 +0200

    Dodana sila za stabiliziranje autica prilikom okreta

--- Assets/Scripts/CarMovement.cs
+++ Assets/Scripts/CarMovement.cs
@@ -7,24 +7,25 @@ public class CarMovement : MonoBehaviour {
 
 	private float thrustTorque;
 	//podatkovni clanovi klase za kretanje autica
-	public float speed = 15.0f;
+	public float speed = 150.0f;
 	/*	Kola i tijelo auta moraju biti sinkronizirana
 	 *  inace će se kola vrtiti krivo
 	 * */
 	private Quaternion wheelRotationToBody;
+	private Rigidbody carBody;
 
 	//svi kotaci u string zapisu
 	private string[] tireNames = new string[4] {
-		"simpleCar/LeftFrontTirePivot/LeftFrontTire",
-		"simpleCar/RightFrontTirePivot/RightFrontTire",
+		"simpleCar/SteerLeftTire/LeftFrontTirePivot/LeftFrontTire",
+		"simpleCar/SteerRightTire/RightFrontTirePivot/RightFrontTire",
 		"simpleCar/LeftBackTirePivot/LeftBackTire",
 		"simpleCar/RightBackTirePivot/RightBackTire",
 	};
 
 	//svi pivoti u string zapisu
 	private string[] tirePivotNames = new string[4]{
-		"simpleCar/LeftFrontTirePivot",
-		"simpleCar/RightFrontTirePivot",
+		"simpleCar/SteerLeftTire/LeftFrontTirePivot",
+		"simpleCar/SteerRightTire/RightFrontTirePivot",
 		"simpleCar/LeftBackTirePivot",
 		"simpleCar/RightBackTirePivot"
 	};
@@ -37,6 +38,8 @@ public class CarMovement : MonoBehaviour {
 		"WheelColliders/RightBackTireCL",
 	};
 	private WheelCollider[] wheelColliders;
+	private GameObject[] steerWheels;
+	private float AntiRoll = 10000f;
 
 	//niz kotaca i pivota
 	private GameObject[] tires;
@@ -49,50 +52,84 @@ public class CarMovement : MonoBehaviour {
 		this.tires = new GameObject[4];
 		this.tirePivots = new GameObject[4];
 		this.wheelColliders = new WheelCollider[4];
+		this.steerWheels = new GameObject[2];
+
+		this.steerWheels [0] = GameObject.Find ("simpleCar/SteerLeftTire");
+		this.steerWheels [1] = GameObject.Find ("simpleCar/SteerRightTire");
 
 		for (int i = 0; i < 4; i++) {
 			this.tires[i] = GameObject.Find(this.tireNames[i]);
 			this.tirePivots[i] = GameObject.Find(this.tirePivotNames[i]);
 			this.wheelColliders[i] = GameObject.Find(this.wheelColiderNames[i]).GetComponent<WheelCollider>();
 		}
+
+		//spustimo centar mase
+		this.carBody = GameObject.Find ("PlayerHolder").GetComponent<Rigidbody> ();
 	}
 
 
 	private void MoveCar() {
-		WheelHit wheelHit;
-		for(int i = 0; i < 4; i++) {
+		for(int i = 2; i < 4; i++) {
 			this.wheelColliders[i].motorTorque = thrustTorque;
-			//this.wheelColliders[i].GetGroundHit(out wheelHit);
-			//Debug.Log(wheelHit.forwardSlip);
 		}
-	
+
 	}
 
 	private void RotateTheTires(float move) {
 		move = move * 25f;
-		for(int i = 0; i < 4; i++)
-			this.tirePivots[i].transform.Rotate (0f, 0f, this.speed * move * Time.deltaTime);
+		for (int i = 0; i < 4; i++) 
+			this.tirePivots[i].transform.Rotate(Vector3.forward, this.speed * move * Time.deltaTime);			
 	}
 
 	//kocnica
 	private void ApplyBrake() {
 		for (int i = 2; i < 4; i++)
 			this.wheelColliders [i].brakeTorque = 2f;
+	}
 
-		Debug.Log ("Called!");
+	//imitacija "centripetalne" sile 
+	private void centripetalForce(float steer) {
+		WheelHit hit;
+
+		bool groundedL = this.wheelColliders [0].GetGroundHit (out hit);
+		bool groundedR = this.wheelColliders [1].GetGroundHit (out hit);
+
+		if ((!groundedL && !groundedR) || (groundedL && groundedR))
+			return;
+		
+			
+
+		if (!groundedL) {
+			this.carBody.AddForceAtPosition (this.wheelColliders [0].transform.up * -this.AntiRoll, this.wheelColliders [0].transform.position);
+			Debug.Log("Left centripetal");
+		}
+			
+			
+
+		if (!groundedR) {
+			this.carBody.AddForceAtPosition (this.wheelColliders [1].transform.up * -this.AntiRoll, this.wheelColliders [1].transform.position);
+			Debug.Log("Right centripetal");
+		}
+			
+			
 	}
 
 	//skretanje
 	private void SteerCar(float steer) {
+		steer = 45f * steer;
 		for (int i = 0; i < 2; i++) {
-			this.wheelColliders[i].steerAngle = 15f * steer;
+			Quaternion quat;
+			Vector3 pos;
+			this.wheelColliders[i].steerAngle = steer;
+			this.steerWheels[i].transform.localRotation = Quaternion.Euler(0f, 90f + this.wheelColliders[i].steerAngle, 0f);
 		}
+
+		this.centripetalForce (steer);
 	}
 
 
 	//ova se metoda poziva svaki frame
-	public void FixedUpdate()
-	{
+	public void FixedUpdate() {
 		float move = Input.GetAxis ("Vertical");
 
 		if (Input.GetKey(KeyCode.Space))
@@ -100,12 +137,9 @@ public class CarMovement : MonoBehaviour {
 
 		SteerCar (Input.GetAxis ("Horizontal"));	
 		
-
-		Vector3 movement = Vector3.forward * move * this.speed * Time.deltaTime;
-		this.thrustTorque = move * this.speed * 159f;
+		this.thrustTorque = move * this.speed;
 		MoveCar ();
-		//this.transform.Translate (movement);
-		this.RotateTheTires (-move);
+		this.RotateTheTires (-move);	
 	}
 
 
